0855b28e2968aeb82c76a324c432385af22cb629
Akilex123 <aleksajeretin1@gmail.com>
2025-08-25 14:21:57 +0200

    Method for time measurement set up

--- PerformanceAnalyzer.java
+++ PerformanceAnalyzer.java
@@ -1,5 +1,23 @@
 public class PerformanceAnalyzer {
-    public static void main(String args[]){
+    public static long measureTime(Runnable task){
+        long startTime = System.nanoTime();
+        task.run();
+        long endTime = System.nanoTime();
+        return (endTime - startTime)/1000;
+    }
 
+    public static void main(String args[]){
+        long duration = measureTime(()->{
+            Array1();
+        });
+        System.out.println("Ukupno vreme je: " + duration + "ms");
+
+
+    }
+    public static void Array1(){
+        int[] array = new int[1000];
+        for (int i=0; i<array.length;i++){
+            array[i] = i;
+        }
     }
 }
